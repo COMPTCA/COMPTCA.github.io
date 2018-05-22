660b3c97fffaaeaac432f55b679c4dd89cce7e09
COMPTCA <39467048+COMPTCA@users.noreply.github.com>
2018-05-22 15:23:06 -0500

    Add files via upload

--- Tutorials/JavaScript & Jquery/JavaScript & JQuery - Interactive Front End Web Development Book/JQuery/JS/2-2-JQuery.js
+++ Tutorials/JavaScript & Jquery/JavaScript & JQuery - Interactive Front End Web Development Book/JQuery/JS/2-2-JQuery.js
@@ -1,10 +1,10 @@
-var vote = '<div id="vote"><div class="third"><a href="http://example.org?tshirt=gray"><img src="Images/t-gray.png" id="gray" alt="gray" /></a></div><div class="third"><a href="http://example.org?tshirt=yellow" id="yellow"><img src="Images/t-yellow.png" id="yellow" alt="yellow" /></a></div><div class="third"><a href="http://example.org?tshirt=green"><img src="Images/t-green.png" id="green" alt="green" /></a></div></div>';
+var vote = '<div id="vote"><div class="third"><a href="PHP/vote.php?vote=gray"><img src="Images/t-gray.png" id="gray" alt="gray" /></a></div><div class="third"><a href="PHP/vote.php?vote=yellow" id="yellow"><img src="Images/t-yellow.png" id="yellow" alt="yellow" /></a></div><div class="third"><a href="PHP/vote.php?vote=green"><img src="Images/t-green.png" id="green" alt="green" /></a></div></div>';
 $('#selector').append(vote);
 
-$('#selector a').on('click', function(e){
-	e.preventDefault();
-	var queryString = 'vote=' + $(e.target).attr('id');
-	$.get('/PHP/votes.php', queryString, function(data){
-		$('#selector').html(data);
-	});
+$('#selector a').on('click', function(e) {
+  	e.preventDefault();
+  	var queryString = 'vote=' + $(e.target).attr('id');
+  	$.get('PHP/votes.php', queryString, function(data) {
+    	$('#selector').html(data);
+  	});
 });
