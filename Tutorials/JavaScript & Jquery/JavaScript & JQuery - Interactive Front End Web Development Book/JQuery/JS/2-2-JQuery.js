0b881e6def42098eea686e0d9280844d1d148f2c
COMPTCA <39467048+COMPTCA@users.noreply.github.com>
2018-05-22 20:25:10 -0500

    Add files via upload

--- Tutorials/JavaScript & Jquery/JavaScript & JQuery - Interactive Front End Web Development Book/JQuery/JS/2-2-JQuery.js
+++ Tutorials/JavaScript & Jquery/JavaScript & JQuery - Interactive Front End Web Development Book/JQuery/JS/2-2-JQuery.js
@@ -5,7 +5,7 @@ $('#selector').append(vote);
 
 $('#selector a').on('click', function(e){
   	e.preventDefault();
-  	var queryString = 'vote=' + $(e.target).attr('id').text();
+  	var queryString = 'vote=' + $(e.target).attr('id');
   	$.get('votes.php', queryString, function(data){
     	$('#selector').html(data);
   	});
