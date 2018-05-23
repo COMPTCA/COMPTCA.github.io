c92cb1b12896673025191696a13a7460d5636c84
COMPTCA <39467048+COMPTCA@users.noreply.github.com>
2018-05-22 20:16:08 -0500

    Add files via upload

--- Tutorials/JavaScript & Jquery/JavaScript & JQuery - Interactive Front End Web Development Book/JQuery/JS/2-2-JQuery.js
+++ Tutorials/JavaScript & Jquery/JavaScript & JQuery - Interactive Front End Web Development Book/JQuery/JS/2-2-JQuery.js
@@ -1,4 +1,4 @@
-var vote = '<div id="vote"><div class="third"><a href="http://example.org?tshirt=gray"><img src="Images/t-gray.png" id="gray" alt="gray"/></a></div><div class="third"><a href="http://example.org?tshirt=yellow" id="yellow"><img src="Images/t-yellow.png" id="yellow" alt="yellow"/></a></div><div class="third"><a href="http://example.org?tshirt=green"><img src="Images/t-green.png" id="green" alt="green"/></a></div></div>';
+var vote = '';
 $('#selector').append(vote);
 
 $('#selector a').on('click', function(e){
